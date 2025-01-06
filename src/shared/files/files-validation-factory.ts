d2840b4ab61395ce004f7eee7ed2b3527e98a58f
KarimMohamed6 <km6777804@gmail.com>
2025-01-06 23:14:35 +0200

    refact: refactoring part-2

--- src/shared/files/files-validation-factory.ts
+++ src/shared/files/files-validation-factory.ts
@@ -1,27 +1,36 @@
 import {
   FileTypeValidator,
+  FileValidator,
   HttpStatus,
   MaxFileSizeValidator,
   ParseFilePipe,
 } from '@nestjs/common';
 import { FileSignatureValidator } from './validators/file-signature.validator';
 
+const createFileValidator = (
+  maxSize: number,
+  fileType: RegExp | string,
+): FileValidator[] => [
+  //validate file size
+  new MaxFileSizeValidator({
+    maxSize: maxSize,
+    message: (maxSize) =>
+      `File is too large, max file size is ${maxSize} bytes`,
+  }),
+  //validate file type
+  new FileTypeValidator({
+    fileType: fileType,
+  }),
+  //validate file signature
+  new FileSignatureValidator(),
+];
+
 export const CreateParseFilePipe = (
   maxSize: number,
   fileType: RegExp,
 ): ParseFilePipe =>
   new ParseFilePipe({
-    validators: [
-      new MaxFileSizeValidator({
-        maxSize: maxSize,
-        message: (maxSize) =>
-          `File is too large, max file size is ${maxSize} bytes`,
-      }),
-      new FileTypeValidator({
-        fileType: fileType,
-      }),
-      new FileSignatureValidator(),
-    ],
+    validators: createFileValidator(maxSize, fileType),
     errorHttpStatusCode: HttpStatus.UNSUPPORTED_MEDIA_TYPE,
     fileIsRequired: true,
   });
